5661994dff518560f693de668b4f7656c27899b5
St4rLght05 <toadmaster009@gmail.com>
2025-11-16 22:06:37 -0500

    feat: added exercicses for 3 first classes

---
+++ ejercicios/3. Comparadores y Operadores/Comparadores/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int edad = 16;
+		
+		System.out.println(edad > 13); //Mayor que
+		System.out.println(edad < 13); //Menor que
+		System.out.println(edad == 13); //Igual a
+		System.out.println(edad != 13); //Diferente de
+	}
+}
